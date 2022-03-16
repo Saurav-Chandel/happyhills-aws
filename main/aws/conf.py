131f8b477cff3f673b3fe16c964148b091fe22b1
Saurav-Chandel <chandelsaurav817@gmail.con>
2022-03-16 22:41:03 +0530

    g

--- main/aws/conf.py
+++ main/aws/conf.py
@@ -2,6 +2,8 @@
 AWS_ACCESS_KEY_ID = "AKIA2K5WZGM5LPVKVIKY"
 AWS_SECRET_ACCESS_KEY = "biovwSYM8PUJPK1IqkOdpWZHZy3VzQUIFezO7LM"
 AWS_FILE_EXPIRE=2000
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
 AWS_S3_REGION_NAME = "us-east-1"  # e.g. us-east-2
 AWS_STORAGE_BUCKET_NAME = "happyhills-s3"
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
@@ -11,5 +13,5 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = "static"
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-DEFAULT_FILE_STORAGE = "main.storage_backends.MediaStorage"
+STATICFILES_STORAGE = "main.aws.utils.MediaRootS3BotoStorage"
+DEFAULT_FILE_STORAGE = "main.aws.utils.StaticRootS3BotoStorage"
