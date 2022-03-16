192942b773d37f4c2748aafbd5aa287eb614069c
Saurav-Chandel <chandelsaurav817@gmail.con>
2022-03-16 20:45:18 +0530

    s3 False

--- main/settings.py
+++ main/settings.py
@@ -212,7 +212,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-SERVE_FROM_S3 = True
+SERVE_FROM_S3 = False
 
 if not SERVE_FROM_S3:
 
