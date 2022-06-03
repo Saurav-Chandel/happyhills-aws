83c83aaf78de53c2afcfbcd1ce093ef82dc2a644
Saurav-Chandel <chandelsaurav817@gmail.con>
2022-06-04 02:19:28 +0530

    d

--- main/aws/conf.py
+++ main/aws/conf.py
@@ -8,7 +8,7 @@ AWS_QUERYSTRING_AUTH = True
 AWS_S3_REGION_NAME = "us-east-1"  # e.g. us-east-2
 AWS_STORAGE_BUCKET_NAME = "happyhills-s3bucket"
 AWS_DEFAULT_ACL=None
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME   
+# AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME   
 # AWS_S3_CUSTOM_DOMAIN = "d2dr6wwncpi0to.cloudfront.net"
 # AWS_S3_OBJECT_PARAMETERS = {
 #     "CacheControl": "max-age=86400",
@@ -19,8 +19,6 @@ STATICFILES_STORAGE = "main.aws.utils.StaticRootS3BotoStorage"
 DEFAULT_FILE_STORAGE = "main.aws.utils.MediaRootS3BotoStorage"
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
-
-AWS_S3_SIGNATURE_VERSION="s3v4"
 
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_S3_FILE_OVERWRITE = False
