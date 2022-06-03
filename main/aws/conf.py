cc1ef0bc3071d0b95e3d261f537e31cc88b87d41
Saurav-Chandel <chandelsaurav817@gmail.con>
2022-06-04 02:22:40 +0530

    ff

--- main/aws/conf.py
+++ main/aws/conf.py
@@ -18,6 +18,8 @@ AWS_DEFAULT_ACL=None
 STATICFILES_STORAGE = "main.aws.utils.StaticRootS3BotoStorage"
 DEFAULT_FILE_STORAGE = "main.aws.utils.MediaRootS3BotoStorage"
 
+AWS_S3_SIGNATURE_VERSION=""s3v4"
+
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
 
 # AWS_S3_FILE_OVERWRITE = False
