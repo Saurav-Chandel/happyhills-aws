475678fbb7643b9a68c809d66bfd1b5c8651004d
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-04 16:42:03 +0530

    'a'

--- custom_trip/models.py
+++ custom_trip/models.py
@@ -3,9 +3,6 @@ from upcoming_treks.models import Upcoming_Road_Trips
 from package.models import Package
 # Create your models here.
 
-
-
-
 class State(models.Model):
   
     image = models.ImageField(upload_to ='state/',null=True,blank=True)
@@ -13,23 +10,24 @@ class State(models.Model):
     overview=models.CharField(max_length=3000,null=True,blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class District(models.Model):
     image = models.ImageField(upload_to ='district/',null=True,blank=True)
     overview=models.CharField(max_length=3000,null=True,blank=True)
-    name = models.CharField(max_length=200,null=True,blank=True)
-    state=models.ForeignKey(State,on_delete=models.CASCADE, related_name="state")
+    name = models.CharField(max_length=100,null=True,blank=True)
+    state=models.ForeignKey(State,on_delete=models.CASCADE, related_name="state",null=True,blank=True)
     route = models.CharField(max_length=200,null=True,blank=True)
     altitude = models.CharField(max_length=200,null=True,blank=True)
     trek_length = models.CharField(max_length=100,null=True,blank=True)
     duration = models.CharField(max_length=100,null=True,blank=True)
     best_season = models.CharField(max_length=100,null=True,blank=True)
     difficulty_level = models.CharField(max_length=100,null=True,blank=True)
+    
 
     def __str__(self):
-        return str(self.name)
+        return self.name
 
 
 class Destination(models.Model):
@@ -49,6 +47,5 @@ class Destination(models.Model):
     
 
     def __str__(self):
-        return str(self.name)
+        return self.name
 
-        
